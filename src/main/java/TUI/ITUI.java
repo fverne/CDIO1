6834d19b697d9f2bd85cee5a61a6b1a4037db5ac
Jacob Berg Eriksen <jacob1211@hotmail.com>
2020-03-11 15:45:30 +0100

    addUser in TUI fixed check-methods in Controller added

--- src/main/java/TUI/ITUI.java
+++ src/main/java/TUI/ITUI.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public interface ITUI {
 
     void showMenu() throws IOException, IUserDAO.DALException;
-    void addUser();
+    void addUser() throws IUserDAO.DALException;
     void showUser();
     void updateUser() throws IUserDAO.DALException;
     void deleteUser() throws IUserDAO.DALException;
