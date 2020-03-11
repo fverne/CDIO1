0b5d820cbae332b9e23e2d66d7c157c662ddd14b
Jonas Henriksen <56156281+JonasHenriksen123@users.noreply.github.com>
2020-03-11 12:01:43 +0100

    Update IController.java

--- src/main/java/funktionalitet/IController.java
+++ src/main/java/funktionalitet/IController.java
@@ -1,4 +1,19 @@
 package funktionalitet;
 
-public interface IController {
+import data.IUserDAO;
+import datatransfer.UserDTO;
+
+import java.io.IOException;
+
+interface iController {
+
+
+    void addUser(UserDTO user) throws IUserDAO.DALException;
+
+    UserDTO showUser(int userId) throws IUserDAO.DALException;
+
+    void deleteUser(int userId) throws IOException, IUserDAO.DALException;
+
+    void updateUser(int userId) throws IUserDAO.DALException;
+
 }
