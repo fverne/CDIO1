708aa46fd2bcb62db462b45dde82b803af3b86d3
Mads Storgaard-Nielsen <s180076@win.dtu.dk>
2020-03-11 07:25:25 +0100

    -

--- src/main/java/funktionalitet/iController.java
+++ src/main/java/funktionalitet/iController.java
@@ -1,20 +1,4 @@
 package funktionalitet;
 
-import data.IUserDAO;
-import datatransfer.UserDTO;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-
-public interface iController {
-
-    void addUser() throws IOException;
-
-    void showUser(String ID) throws IUserDAO.DALException;
-
-    void deleteUser(String ID) throws IOException;
-
-    void editUser(UserDTO user);
+public interface IController {
 }
